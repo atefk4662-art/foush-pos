6078461972e2d977c9976cce8f3572207f3c5ee8
atef askalany <atefk4662@gmail.com>
2026-06-01 13:03:33 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,18 @@
-const CACHE_NAME = 'foush-v8';
-const ASSETS = [
-  './',
-  './index.html',
-  './logo.png',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700;900&family=Orbitron:wght@400;900&display=swap',
-  'https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/10.12.0/firebase-database-compat.js'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', (e) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Caching PWA assets...');
-      return cache.addAll(ASSETS);
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => caches.delete(key)));
+    }).then(() => {
+      self.registration.unregister();
     })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  // Do nothing, let the browser handle it naturally
 });
 
-self.addEventListener('fetch', event => {
-  // Network First, fallback to cache for offline capabilities
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
-});
