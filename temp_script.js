a6d5e5f3f33267d6ff881d0189eaa810e7504675
atef askalany <atefk4662@gmail.com>
2026-06-01 13:25:41 +0300

    Delete temp_script.js

--- temp_script.js
+++
@@ -1,6 +0,0 @@
-
-        window.onerror = function (msg, url, lineNo, columnNo, error) {
-            document.body.innerHTML = '<div style="color:white; background:red; padding:20px; z-index:999999; position:fixed; top:0; left:0; width:100%; height:100%; overflow:auto;"><h2>CRITICAL ERROR</h2><p>' + msg + '</p><p>Line: ' + lineNo + '</p><pre>' + (error && error.stack ? error.stack : '') + '</pre></div>';
-            return false;
-        };
-    
